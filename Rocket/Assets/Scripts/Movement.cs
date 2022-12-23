8da7aacb103de5aa859d37cf1177b2f65cfa64e1
ikhalod <121309226+ikhalod@users.noreply.github.com>
2022-12-23 21:51:20 +0300

    Rocket Project

---
+++ Rocket/Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float speed = 100f;
+    [SerializeField] float direction = 5f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+        ProcessRotation();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddRelativeForce(Vector3.up * Time.deltaTime * speed);
+            Debug.Log("Boost Activated");
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            ApplyRotation(direction);
+            Debug.Log("Rotating to the left!");
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ApplyRotation(-direction);
+            Debug.Log("Rotating to the Right!");
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // Freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotationThisFrame);
+        rb.freezeRotation = false; // Unfreezing rotation so we can play normally
+    }
+}
