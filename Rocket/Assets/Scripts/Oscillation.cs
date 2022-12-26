54f5243e6d7d398c10f4bd46afb2f3c29538e255
Khalid Alyahya <ikhalod@gmail.com>
2022-12-26 17:09:08 +0300

    Completed

---
+++ Rocket/Assets/Scripts/Oscillation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillation : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 newPosition;
+    [SerializeField] float speed = 2f;
+
+    float movementFactor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / speed;
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSinWave + 1f) / 2;
+
+        Vector3 offset = newPosition * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
